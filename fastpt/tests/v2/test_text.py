3431029161a4362ebf0ccf60af04d4c3aeae98ec
Rick Copeland <rick446@usa.net>
2010-09-09 10:59:10 -0400

    Text template expr substitution support

--- fastpt/tests/v2/test_text.py
+++ fastpt/tests/v2/test_text.py
@@ -4,11 +4,17 @@ from fastpt.v2.text import TextTemplate
 
 class TestBasic(TestCase):
 
-    def setUp(self):
-        self.tpl = TextTemplate(source='Hello, ${name}\n')
-
     def test(self):
-        rsp = self.tpl(dict(name='Rick')).__fpt__.render() 
+        tpl = TextTemplate(source='Hello, ${name}\n')
+        rsp = tpl(dict(name='Rick')).__fpt__.render() 
+        assert rsp == 'Hello, Rick\n', rsp
+        tpl = TextTemplate(source='Hello, $name\n')
+        rsp = tpl(dict(name='Rick')).__fpt__.render() 
+        assert rsp == 'Hello, Rick\n', rsp
+        tpl = TextTemplate(source='Hello, $obj.name\n')
+        class Empty: pass
+        Empty.name = 'Rick'
+        rsp = tpl(dict(obj=Empty)).__fpt__.render() 
         assert rsp == 'Hello, Rick\n', rsp
 
 if __name__ == '__main__':
