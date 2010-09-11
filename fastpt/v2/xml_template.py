461dc358ce8413937d80c584fee1f3a283a2a53d
Rick Copeland <rick446@usa.net>
2010-09-10 23:37:21 -0400

    Trace line numbers in xml, keep comments in the DOM

--- fastpt/v2/xml_template.py
+++ fastpt/v2/xml_template.py
@@ -23,24 +23,27 @@ class _Parser(sax.ContentHandler):
     def __init__(self, filename, source):
         self._filename = filename
         self._source = source
-        self._dom = None
+        self._doc = None
         self._els = []
 
     def parse(self):
-        parser = sax.make_parser()
+        self._parser = parser = sax.make_parser()
         parser.setFeature(sax.handler.feature_external_pes, False)
         parser.setFeature(sax.handler.feature_external_ges, False)
         parser.setFeature(sax.handler.feature_namespaces, False)
+        parser.setProperty(sax.handler.property_lexical_handler, self)
         parser.setContentHandler(self)
         parser.parse(StringIO(self._source))
         return self._doc
 
+    ## ContentHandler implementation
     def startDocument(self):
         self._doc = dom.Document()
         self._els.append(self._doc)
 
     def startElement(self, name, attrs):
         el = self._doc.createElement(name)
+        el.lineno = self._parser.getLineNumber()
         for k,v in attrs.items():
             el.setAttribute(k,v)
         self._els[-1].appendChild(el)
@@ -50,29 +53,41 @@ class _Parser(sax.ContentHandler):
         popped = self._els.pop()
         assert name == popped.tagName
 
-    def startElementNS(self, name, qname, attrs): # pragma no cover
-        raise NotImplementedError, 'startElementNS'
-
-    def endElementNS(self, name, qname):# pragma no cover
-        raise NotImplementedError, 'startElementNS'
-
     def characters(self, content):
         node = self._doc.createTextNode(content)
         self._els[-1].appendChild(node)
 
     def processingInstruction(self, target, data):
         node = self._doc.createProcessingInstruction(target, data)
+        node.lineno = self._parser.getLineNumber()
         self._els[-1].appendChild(node)
 
     def skippedEntity(self, name):
         content = unicode(HTMLParser.entitydefs[name], 'latin-1')
         return self.characters(content)
 
+    def startElementNS(self, name, qname, attrs): # pragma no cover
+        raise NotImplementedError, 'startElementNS'
+
+    def endElementNS(self, name, qname):# pragma no cover
+        raise NotImplementedError, 'startElementNS'
+
     def startPrefixMapping(self, prefix, uri):# pragma no cover
         raise NotImplemented, 'startPrefixMapping'
 
     def endPrefixMapping(self, prefix):# pragma no cover
         raise NotImplemented, 'endPrefixMapping'
+
+    # LexicalHandler implementation
+    def comment(self, text):
+        node = self._doc.createComment(text)
+        node.lineno = self._parser.getLineNumber()
+        self._els[-1].appendChild(node)
+
+    def startCDATA(self): pass
+    def endCDATA(self): pass
+    def startDTD(self, name, pubid, sysid): pass
+    def endDTD(self): pass
 
 def expand(tree, parent=None):
     if not isinstance(tree.tagName, basestring): return tree
@@ -87,6 +102,7 @@ def expand(tree, parent=None):
         tree.removeAttribute(directive)
         # nsmap = (parent is not None) and parent.nsmap or tree.nsmap
         el = tree.ownerDocument.createElement(directive)
+        el.lineno = tree.lineno
         if attr:
             el.setAttribute(attr, value)
         # el.setsourceline = tree.sourceline
