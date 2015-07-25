6deed86c64d9794e007ebd859453773627325b42
Alessandro Molina <alessandro.molina@axant.it>
2015-07-25 17:13:17 +0200

    Update version file after release

--- kajiki/version.py
+++ kajiki/version.py
@@ -2,5 +2,5 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-__version__ = '0.4'
-__release__ = '0.5.1dev1'
+__version__ = '0.5'
+__release__ = '0.5.1dev'
