d37d3b083ba4788819bbdc08481331967bc323ad
Alessandro Molina <alessandro.molina@axant.it>
2016-11-27 11:41:47 +0100

    Close tags that have a text node or children, it's usually expected they are

--- kajiki/html_utils.py
+++ kajiki/html_utils.py
@@ -17,29 +17,14 @@ HTML_EMPTY_ATTRS = set([
 HTML_OPTIONAL_END_TAGS = set([
     'area',
     'base',
-    'body',
     'br',
     'col',
-    'colgroup',
-    'dd',
-    'dt',
-    'head',
     'hr',
-    'html',
     'img',
     'input',
-    'li',
     'link',
     'meta',
-    'option',
-    'p',
-    'param',
-    'tbody',
-    'td',
-    'tfoot',
-    'th',
-    'thead',
-    'tr',
+    'param'
 ])
 HTML_REQUIRED_END_TAGS = set(['script'])
 HTML_CDATA_TAGS = set(('script', 'style'))
