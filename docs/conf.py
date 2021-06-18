d38bfcbe28d2de6a8c0a10d0e57922a33dd12de2
Jack Rosenthal <jack@rosenth.al>
2021-06-17 18:49:34 -0600

    Spiff up docs with alabaster theme and add to travis

--- docs/conf.py
+++ docs/conf.py
@@ -92,12 +92,20 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = dict(
+    description='Fast XML and text templates for Python',
+    logo='marlin.png',
+    logo_name=True,
+    github_banner=True,
+    github_button=True,
+    github_user='jackrosenthal',
+    github_repo='kajiki',
+)
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
--- docs/index.rst
+++ docs/index.rst
@@ -3,10 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-.. image:: media/Logo.png
-
-Welcome to Kajiki's documentation!
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Kajiki: Fast Templates for Python
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .. include:: ../README.rst
 
