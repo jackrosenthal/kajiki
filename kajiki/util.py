2fd25390af60194ff91a4f15a86193140eac2e66
Jack Rosenthal <jack@rosenth.al>
2021-06-20 15:50:42 -0600

    kajiki.util: clean up unused definitions
    Lots of unused stuff lurking around here

--- kajiki/util.py
+++ kajiki/util.py
@@ -1,43 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from collections import deque
-import sys
 from random import randint
 from threading import local
-
-
-def debug():  # pragma no cover
-    def pm(etype, value, tb):
-        import pdb
-        import traceback
-        try:
-            from IPython.ipapi import make_session
-            make_session()
-            from IPython.Debugger import Pdb
-            sys.stderr.write('Entering post-mortem IPDB shell\n')
-            p = Pdb(color_scheme='Linux')
-            p.reset()
-            p.setup(None, tb)
-            p.print_stack_trace()
-            sys.stderr.write('%s: %s\n' % (etype, value))
-            p.cmdloop()
-            p.forget()
-            # p.interaction(None, tb)
-        except ImportError:
-            sys.stderr.write('Entering post-mortem PDB shell\n')
-            traceback.print_exception(etype, value, tb)
-            pdb.post_mortem(tb)
-    sys.excepthook = pm
 
 
 def expose(func):
     func.exposed = True
     return func
-
-
-class Undefined(object):
-    pass
-UNDEFINED = Undefined()
 
 
 class flattener(object):
