5f26691890982c71eb538e4b35cba930c089f4c5
Jack Rosenthal <jack@rosenth.al>
2021-06-20 16:36:34 -0600

    Drop usages of nine.native_str

--- kajiki/xml_template.py
+++ kajiki/xml_template.py
@@ -8,8 +8,6 @@ from xml import sax
 from xml.dom import minidom as dom
 from xml.sax import SAXParseException
 
-from nine import native_str
-
 from . import ir
 from . import template
 from .doctype import DocumentTypeDeclaration, extract_dtd
@@ -615,7 +613,7 @@ class _Parser(sax.ContentHandler):
         # So if source is unicode, we pre-encode it:
         # TODO Is this dance really necessary? Can't I just call a function?
         byts = self._source.encode('utf-8')
-        source.setEncoding(native_str('utf-8'))
+        source.setEncoding('utf-8')
         source.setByteStream(io.BytesIO(byts))
         source.setSystemId(self._filename)
 
