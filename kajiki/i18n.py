5da9704664eedb7ccf8936741645dc4e79db697d
Jack Rosenthal <jack@rosenth.al>
2021-06-22 17:48:34 -0600

    Format all files with Black

--- kajiki/i18n.py
+++ kajiki/i18n.py
@@ -15,28 +15,33 @@ def extract(fileobj, keywords, comment_tags, options):
 
     try:
         from babel.messages.extract import extract_python
-        extract_expr = options.get('extract_python', False)
+
+        extract_expr = options.get("extract_python", False)
     except ImportError:
         extract_python = None
         extract_expr = False
 
     source = fileobj.read()
     if isinstance(source, bytes):
-        source = source.decode('utf-8')
-    doc = _Parser(filename='<string>', source=source).parse()
-    doc = _DomTransformer(doc, strip_text=options.get('strip_text', False)).transform()
-    compiler = _Compiler(filename='<string>', doc=doc,
-                         mode=options.get('mode', 'xml'),
-                         is_fragment=options.get('is_fragment', False))
+        source = source.decode("utf-8")
+    doc = _Parser(filename="<string>", source=source).parse()
+    doc = _DomTransformer(doc, strip_text=options.get("strip_text", False)).transform()
+    compiler = _Compiler(
+        filename="<string>",
+        doc=doc,
+        mode=options.get("mode", "xml"),
+        is_fragment=options.get("is_fragment", False),
+    )
     ir = compiler.compile()
     for node in ir:
         if isinstance(node, TranslatableTextNode):
             if node.text.strip():
-                yield (node.lineno, '_', node.text, [])
+                yield (node.lineno, "_", node.text, [])
         elif extract_expr and isinstance(node, ExprNode):
             try:
-                for e in extract_python(BytesIO(node.text.encode('utf-8')),
-                                        keywords, comment_tags, options):
+                for e in extract_python(
+                    BytesIO(node.text.encode("utf-8")), keywords, comment_tags, options
+                ):
                     yield (node.lineno, e[1], e[2], e[3])
             except (TokenError, SyntaxError) as e:
-                raise KajikiSyntaxError(e, source, '<string>', node.lineno, 0)
+                raise KajikiSyntaxError(e, source, "<string>", node.lineno, 0)
--- speedtest.py
+++ speedtest.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 from kajiki import XMLTemplate
 from genshi.template import MarkupTemplate
 
-FN = 'kajiki/perf/tables.html'
+FN = "kajiki/perf/tables.html"
 
 timings = defaultdict(float)
 
@@ -15,33 +15,40 @@ def timing(s):
     start = time.time()
     yield
     elapsed = time.time() - start
-    print('%s: %s s' % (s, elapsed))
+    print("%s: %s s" % (s, elapsed))
     timings[s] += elapsed
 
-with timing('compile.kajiki'):
+
+with timing("compile.kajiki"):
     fpt = XMLTemplate(filename=FN)
     # fpt.compile()
-with timing('compile.genshi'):
+with timing("compile.genshi"):
     gt = MarkupTemplate(open(FN))
-with timing('render.100.kajiki'):
+with timing("render.100.kajiki"):
     fpt(dict(size=100)).render()
-with timing('render.100.genshi'):
+with timing("render.100.genshi"):
     gt.generate(size=100).render()
-with timing('render.100.kajiki'):
+with timing("render.100.kajiki"):
     fpt(dict(size=100)).render()
-with timing('render.100.genshi'):
+with timing("render.100.genshi"):
     gt.generate(size=100).render()
-with timing('render.100.kajiki'):
+with timing("render.100.kajiki"):
     fpt(dict(size=100)).render()
-with timing('render.100.genshi'):
+with timing("render.100.genshi"):
     gt.generate(size=100).render()
-with timing('render.500.kajiki'):
+with timing("render.500.kajiki"):
     fpt(dict(size=500)).render()
-with timing('render.500.genshi'):
+with timing("render.500.genshi"):
     gt.generate(size=500).render()
-print('Compile kajiki speedup: %s' % (
-    timings['compile.genshi'] / timings['compile.kajiki']))
-print('Render 100 kajiki speedup: %s' % (
-    timings['render.100.genshi'] / timings['render.100.kajiki']))
-print('Render 500 kajiki speedup: %s' % (
-    timings['render.500.genshi'] / timings['render.500.kajiki']))
+print(
+    "Compile kajiki speedup: %s"
+    % (timings["compile.genshi"] / timings["compile.kajiki"])
+)
+print(
+    "Render 100 kajiki speedup: %s"
+    % (timings["render.100.genshi"] / timings["render.100.kajiki"])
+)
+print(
+    "Render 500 kajiki speedup: %s"
+    % (timings["render.500.genshi"] / timings["render.500.kajiki"])
+)
