ec6c9aed3d71dac8fb817b28d28a5abeba46f304
Jack Rosenthal <jack@rosenth.al>
2021-06-22 16:50:17 -0600

    Add e2e force mode test

--- kajiki/tests/test_e2e.py
+++ kajiki/tests/test_e2e.py
@@ -31,3 +31,33 @@ def test_golden_file(args, golden_file, capsys):
 def test_file_not_found():
     with pytest.raises(IOError):
         main(["/does/not/exist.txt"])
+
+
+# We should be able to force a non-txt file into text mode.
+def test_force_text_mode(tmpdir, capsys):
+    tmpfile = str(tmpdir / "myfile.png")
+    with open(tmpfile, "w") as f:
+        f.write("<!DOCTYPE html>\n")
+        f.write("%for i in range(10)\n")
+        f.write("${i}\n")
+        f.write("%end")
+
+    main(["-m", "text", tmpfile])
+
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == """<!DOCTYPE html>
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+"""
+    )
+    assert captured.err == ""
