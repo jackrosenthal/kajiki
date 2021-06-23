e5c2de2b5f23736b830e58f908eaeb05a57ebb8f
Jack Rosenthal <jack@rosenth.al>
2021-06-22 18:47:00 -0600

    docs/conf.py: drop unused imports
    This makes flake8 happy

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
 from kajiki import version as _version
 
 # If extensions (or modules to document with autodoc) are in another directory,
