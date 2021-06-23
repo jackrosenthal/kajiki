b70d1b6b7d762c31a3539b2cc0a59eec90d3aa8c
Jack Rosenthal <jack@rosenth.al>
2021-06-22 18:16:33 -0600

    Add __all__ to __init__.py to expose the public API
    This makes flake8 happy on this file

--- kajiki/__init__.py
+++ kajiki/__init__.py
@@ -1,6 +1,21 @@
+"""Kajiki public API."""
+
 from .util import expose, flattener
 from .template import Template
 from .loader import MockLoader, FileLoader, PackageLoader
 from .text import TextTemplate
 from .xml_template import XMLTemplate
 from .version import __version__, __release__
+
+__all__ = [
+    "expose",
+    "flattener",
+    "Template",
+    "MockLoader",
+    "FileLoader",
+    "PackageLoader",
+    "TextTemplate",
+    "XMLTemplate",
+    "__version__",
+    "__release__",
+]
