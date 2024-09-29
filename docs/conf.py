459bd6adba809ff94acd654467efcf10c35b3168
Jack Rosenthal <jack@rosenth.al>
2024-09-29 01:02:33 -0600

    docs: Switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -101,19 +101,15 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "description": "Fast XML and text templates for Python",
-    "logo": "marlin.png",
-    "logo_name": True,
-    "github_banner": True,
-    "github_button": True,
-    "github_user": "jackrosenthal",
-    "github_repo": "kajiki",
+    "source_repository": "https://github.com/jackrosenthal/kajiki",
+    "source_branch": "master",
+    "source_directory": "docs/",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -128,12 +124,12 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = "_static/marlin.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "favicon.ico"
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
